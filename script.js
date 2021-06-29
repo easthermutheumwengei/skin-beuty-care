afeeebb17b1fcffe0704b023243b6ccb9a84c699
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-06-29 17:10:03 +0300

    js functions and objects

---
+++ script.js
@@ -0,0 +1,22 @@
+function skinType(oily, dry, normal) {
+    this.oily = oily;
+    this.dry = dry;
+    this.normal = normal;
+
+}
+
+function skinComplexion(dark, light) {
+    this.dark = dark;
+    this.light = light;
+}
+
+//beauty products for dark skin complexion
+beautyProductsDarkOily = ["soothing serum", "iunik", "panoxyl", "coconut water cream"];
+beautyProductsDarkDry = ["pure aloe vera gel", "coconut oil", "triple cream", "eucerin", "borage"];
+beautyProductsDarkNormal = ["cetaphil", "olay", "cerave", "purity cleanser", "baby brown clarins"];
+
+//beauty products for lightskin complexion
+beautyProductsLightOily = [""]
+
+//beauty product location
+beautyProductsDarkOilyLocation = [""]
