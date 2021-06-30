4e26a81f87c6464e2e5f9ebd8c9209fbdec0f73e
C0d3president <kadimasam@gmail.com>
2021-07-01 02:31:53 +0300

    Change key names

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function FindBeautyProducts(skinComplexion, skinType){
 
 FindBeautyProducts.prototype.darkSkinBeautyProducts = {
     
-    beautyProductsDarkOily:  ["soothing serum", "iunik", "panoxyl", "coconut water cream"],
-    beautyProductsDarkDry : ["pure aloe vera gel", "coconut oil", "triple cream", "eucerin", "borage"],
-    beautyProductsDarkNormal : ["cetaphil", "olay", "cerave", "purity cleanser", "baby brown clarins"]
+    oilySkin:  ["soothing serum", "iunik", "panoxyl", "coconut water cream"],
+    drySkin : ["pure aloe vera gel", "coconut oil", "triple cream", "eucerin", "borage"],
+    normalSkin : ["cetaphil", "olay", "cerave", "purity cleanser", "baby brown clarins"]
 
 }
 
@@ -20,27 +20,27 @@ FindBeautyProducts.prototype.darkSkinBeautyProducts = {
 
 FindBeautyProducts.prototype.lightSkinBeautyProducts = {
      
-    beautyProductsLightOily : ["Covergirl Clean Fresh Pressed Powder", "Kate Somerville Oil-Free Moisturizer", "Caudalie Vinopure Natural Salicylic Acid Pore Minimizing Serum", "Dermalogica Oil Free Matte SPF30", "Mary Kay Oil Mattifier" ],
-    beautyProductsLightDry : ["beautyProductsLightOily", "Aquaphor Healing Ointment", "CeraVe Moisturizing Cream", "Vanicream Moisturizing Skin Cream", "CeraVe Facial Moisturizing Lotion PM", "CeraVe SA Cream"],
-    beautyProductsLightNormal : ["Stone Crop Gel Wash", " Lemon Cleanser", "Stone Crop Cleansing Oil"]
+    oilySkin : ["Covergirl Clean Fresh Pressed Powder", "Kate Somerville Oil-Free Moisturizer", "Caudalie Vinopure Natural Salicylic Acid Pore Minimizing Serum", "Dermalogica Oil Free Matte SPF30", "Mary Kay Oil Mattifier" ],
+    drySkin : ["beautyProductsLightOily", "Aquaphor Healing Ointment", "CeraVe Moisturizing Cream", "Vanicream Moisturizing Skin Cream", "CeraVe Facial Moisturizing Lotion PM", "CeraVe SA Cream"],
+    normalSkin : ["Stone Crop Gel Wash", " Lemon Cleanser", "Stone Crop Cleansing Oil"]
 
 }
 // prototype object of location for darkskin
 
 FindBeautyProducts.prototype.locationForDarkSkinProduct = {
 
-    beautyProductsDarkOilyLocation : ["Beauty Click, Parklands, Nairobi", "Reones Beauty and Cosmetics Supply, Eastleigh, Nairobi", "Skincare, Kileleshwa, Nairobi"],
-    beautyProductsDarkDryLocation : ["beauty blog kenya,cbd", "radiant beauty world,nairobi", "suzie beauty,cbd"],
-    beautyProductsDarkNormalLocation : ["le-fremms beauty salon,cbd", "lintons beauty", "beautine enterprises", "brivys beauty products and accessories"]
+    oilySkin : ["Beauty Click, Parklands, Nairobi", "Reones Beauty and Cosmetics Supply, Eastleigh, Nairobi", "Skincare, Kileleshwa, Nairobi"],
+    drySkin : ["beauty blog kenya,cbd", "radiant beauty world,nairobi", "suzie beauty,cbd"],
+    normalSkin : ["le-fremms beauty salon,cbd", "lintons beauty", "beautine enterprises", "brivys beauty products and accessories"]
 }
 
 //  prototype object of location for lightskin
 
 FindBeautyProducts.prototype.locationForLightSkinProduct = {
 
-    beautyProductsLightOilyLocation : ["skincare co.ke ,cbd", "super cosmetics-adams arcade"],
-    beautyProductsLightOilyLocation : ["peepy beauty products,nairobi city", "reones beauty and cosmetics supply,starehe"],
-    beautyProductsLightOilyLocation : ["true cosmetics,mombasa", "markay products ,nairobi"]
+    oilySkin : ["skincare co.ke ,cbd", "super cosmetics-adams arcade"],
+    drySkin : ["peepy beauty products,nairobi city", "reones beauty and cosmetics supply,starehe"],
+    normalSkin : ["true cosmetics,mombasa", "markay products ,nairobi"]
 
 }
 
@@ -67,10 +67,10 @@ $(document).ready(function(){
                 <h2>We suggest the following beauty products for <em>dark</em> skin complexion and <em>oily</em> skin type</h2>
                 <br>
                 <ul>
-                    <li>${findBeautyProducts.darkSkinBeautyProducts. beautyProductsDarkOily[0]}</li>
-                    <li>${findBeautyProducts.darkSkinBeautyProducts. beautyProductsDarkOily[1]}</li>
-                    <li>${findBeautyProducts.darkSkinBeautyProducts. beautyProductsDarkOily[2]}</li>
-                    <li>${findBeautyProducts.darkSkinBeautyProducts. beautyProductsDarkOily[3]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.oilySkin[0]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.oilySkin[1]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.oilySkin[2]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.oilySkin[3]}</li>
                 </ul>
                 <br>
 
@@ -79,15 +79,82 @@ $(document).ready(function(){
                 <br>
 
                 <ul>
-                    <li>${findBeautyProducts.locationForDarkSkinProduct.beautyProductsDarkOilyLocation[0]}</li>
-                    <li>${findBeautyProducts.locationForDarkSkinProduct.beautyProductsDarkOilyLocation[1]}</li>
-                    <li>${findBeautyProducts.locationForDarkSkinProduct.beautyProductsDarkOilyLocation[2]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.oilySkin[0]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.oilySkin[1]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.oilySkin[2]}</li>
                     
                 </ul>
 
 
-        `
-       $(".results").html(results)
+            `
+            $(".results").html(results)
+
+        }
+
+
+         // DARK DRY SKIN
+
+         if (skinComplexion == "dark" && skinType == "dry"){
+
+            const results = `
+                <h2>We suggest the following beauty products for <em>dark</em> skin complexion and <em>oily</em> skin type</h2>
+                <br>
+                <ul>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[0]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[1]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[2]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[3]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[4]}</li>
+                    
+                </ul>
+                <br>
+
+                <h2>We suggest the following locations</h2>
+
+                <br>
+
+                <ul>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[0]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[1]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[2]}</li>
+                </ul>
+
+
+            `
+            $(".results").html(results)
+
+        }
+
+        // DARK NORMAL SKIN
+
+        if (skinComplexion == "dark" && skinType == "normal"){
+
+            const results = `
+                <h2>We suggest the following beauty products for <em>dark</em> skin complexion and <em>oily</em> skin type</h2>
+                <br>
+                <ul>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[0]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[1]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[2]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[3]}</li>
+                    <li>${findBeautyProducts.darkSkinBeautyProducts.drySkin[4]}</li>
+                    
+                </ul>
+                <br>
+
+                <h2>We suggest the following locations</h2>
+
+                <br>
+
+                <ul>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[0]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[1]}</li>
+                    <li>${findBeautyProducts.locationForDarkSkinProduct.drySkin[2]}</li>
+                </ul>
+
+
+            `
+            $(".results").html(results)
 
         }
 
